9609e4f4032a0ac35bfbef53364754fd4074c3e2
Marcos Antonio Perez Villegas <marcos.pvillegas@alumnos.udg.mx>
2022-01-26 13:19:00 -0600

    Archivos del proyecto

---
+++ app/src/main/java/com/example/marcos_antonio_perez_villegas_primeraapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.marcos_antonio_perez_villegas_primeraapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    //Texto de ejemplo
+    }
+}
